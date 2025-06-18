e6304c8bb7698028d1b706d786b79eeab247b81e
RenanChisas <th3kinggameplays@gmail.com>
2025-06-18 01:16:57 -0300

    config build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
-import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Projects } from "./Pages/Projects";
+import { About } from "./Pages/About";
+
+import "./styles/global.css";
+import { JournalMD } from "./Pages/JournalMD";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { StrictMode } from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Projects />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    element: <JournalMD />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
